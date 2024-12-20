7344d94af9064e2b8630cb7ede2a29d4d18485b8
Hari <dnshariprasad@gmail.com>
2024-12-20 08:47:36 +0530

    json response

--- server.js
+++ server.js
@@ -3,8 +3,27 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+const users = [
+  {
+    id: 1,
+    name: "A",
+  },
+  {
+    id: 2,
+    name: "B",
+  },
+  {
+    id: 3,
+    name: "C",
+  },
+  {
+    id: 4,
+    name: "D",
+  },
+];
+
 app.get("/", (req, res) => {
-  res.send("Hello from Express!");
+  res.send(users);
 });
 
 app.listen(PORT, () => {
